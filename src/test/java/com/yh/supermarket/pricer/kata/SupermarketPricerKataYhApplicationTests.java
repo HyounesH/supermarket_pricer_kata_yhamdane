ae3886fcba57e773c1445162c531073ee713a6a5
Younes Hamdane <younes.hamdane11@gmail.com>
2023-03-13 20:53:34 +0100

    add unit test to implements

--- src/test/java/com/yh/supermarket/pricer/kata/SupermarketPricerKataYhApplicationTests.java
+++ src/test/java/com/yh/supermarket/pricer/kata/SupermarketPricerKataYhApplicationTests.java
@@ -1,14 +1,22 @@
 package com.yh.supermarket.pricer.kata;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SupermarketPricerKataYhApplicationTests {
-	@Test
-	void contextLoads() {
-        Assertions.assertTrue(true);
-	}
+    @Test
+    public void test_bask_total_with_no_items(){
 
+    }
+
+    @Test
+    public void test_basket_total_with_items() {
+
+    }
+
+    @Test
+    public void test_basket_total_after_increasing_quantity_of_items(){
+
+    }
 }
